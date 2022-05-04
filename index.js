1a8bc2052a94db080695550c8eb580a6565ed1e2
Dorian Masquelier <dorian.masquelier@gmail.com>
2022-05-04 23:32:41 +0200

    ajout du backend de l'app UberEat

--- index.js
+++ index.js
@@ -5,22 +5,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 //connexion à la bdd
-mongoose.connect("mongodb://localhost/ubereat");
-// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 //création du serveur
 const app = express();
 app.use(cors());
 app.use(formidable());
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:4000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.get("/", (req, res) => {
   res.json("Welcome on Ubereat API !");
@@ -30,12 +20,12 @@ app.get("/test", (req, res) => {
   res.json("Test successful");
 });
 
-//import des routes users et offers
+//import des routes users et orders
 const usersRoutes = require("./routes/users");
 app.use(usersRoutes);
 const ordersRoutes = require("./routes/orders");
 app.use(ordersRoutes);
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server has started !");
 });
